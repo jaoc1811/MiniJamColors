6b4cdd9f73d4a25dceb9b336e21ce2602cabdf18
Juan Oropeza <jaoc1811@gmail.com>
2022-07-22 11:41:53 -0400

    enemy movement

--- Assets/Scripts/SlimesMovement.cs
+++ Assets/Scripts/SlimesMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class SlimesMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Animator bodyAnimator;
+    [SerializeField] Animator shadowAnimator;
+    [SerializeField] Transform target;
+    [SerializeField] Transform transBody;
+    [SerializeField] Vector2 groundVelocity;
+    [SerializeField] float verticalVelocity;
+    [SerializeField] float verticalSpeed;
+    [SerializeField] float speed;
+    [SerializeField] float gravity = -10;
+    [SerializeField] SpriteRenderer bodySprite;
+    [SerializeField] SpriteRenderer shadowSprite;
+
+    bool isGrounded = true;
+
+    private void Start() {
+        StartCoroutine(JumpCorroutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        UpdatePosition();
+        CheckGroundHit();
+    }
+
+    IEnumerator JumpCorroutine() {
+        if (target.position.x < transform.position.x) {
+            bodySprite.flipX = true;
+            shadowSprite.flipX = true;
+        } else {
+            bodySprite.flipX = false;
+            shadowSprite.flipX = false;
+        }
+        yield return new WaitForSeconds(1);
+        bodyAnimator.SetBool("jumping", true);
+        shadowAnimator.SetBool("jumping", true);
+        this.groundVelocity = (target.position - transform.position).normalized * speed;
+        isGrounded = false;
+    }
+
+
+    void UpdatePosition() {
+        if (!isGrounded){
+            transform.position += (Vector3)groundVelocity * Time.deltaTime;
+            verticalVelocity += gravity * Time.deltaTime;
+            transBody.position += new Vector3(0,verticalVelocity,0) * Time.deltaTime;
+        }
+    }
+
+    void CheckGroundHit() {
+        if(transBody.position.y < transform.position.y && !isGrounded) {
+            transBody.position = transform.position;
+            isGrounded = true;
+            bodyAnimator.SetBool("jumping", false);
+            shadowAnimator.SetBool("jumping", false);
+            verticalVelocity = verticalSpeed;
+            StartCoroutine(JumpCorroutine());
+        }
     }
 }
