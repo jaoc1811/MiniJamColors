9ceca5c2d2ec0fc7f4273277b0d8d0111fdc90e2
Carlos Rivero <carlosrivero96@gmail.com>
2022-07-22 14:49:03 -0400

    Player attack

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,35 +7,64 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    Rigidbody2D rb;
+    Vector2 movementVector;
+    bool canAttack = true;
 
-    private float movementX;
-    private float movementY;
-    private Vector2 movementVector;
-
+    [Header ("Movement")]
     [SerializeField] float speed = 2f;
 
-    private float attackDelay = 0.25f;
-    private bool canAttack;
+    [Header ("Attack")]
+    [SerializeField] Transform attackPoint;
+    [SerializeField] float attackRange = 0.33f;
+    [SerializeField] float attackRadius = 0.33f;
+    [SerializeField] float attackDelay = 0.25f;
+    [SerializeField] LayerMask enemyLayers;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate() {
+        rb.velocity = movementVector * speed;
+    }
+
     private void OnMove(InputValue movementValue) {
         movementVector = movementValue.Get<Vector2>();
+        if (movementVector.magnitude != 0) {
+            attackPoint.localPosition = movementVector.normalized * attackRange;
+            attackPoint.localPosition += new Vector3(0, 0.2f, 0);
+        }
     }
 
     private void OnFire() {
         if (canAttack) {
-            canAttack = false;
-            // Swing blade
-            StartCoroutine(attackRecharge());
+            attack();
         }
     }
 
-    private void FixedUpdate() {
-        rb.velocity = movementVector;
+    void attack() {
+        // Play animation
+
+        // Swing blade / detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayers);
+
+            
+
+        // Damage
+        foreach(Collider2D enemy in hitEnemies) {
+            Debug.Log("hit " + enemy.name);
+        }
+        canAttack = false;
+        StartCoroutine(attackRecharge());
+    }
+
+    // Draws hit area in editor
+    private void OnDrawGizmosSelected() {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 
     IEnumerator attackRecharge() {
