7f07e852eb0cd7975180e726d9ad314546495afe
Juan Oropeza <jaoc1811@gmail.com>
2022-07-23 17:24:12 -0400

    added camera zoom script

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    [ContextMenu("ZoomIn")]
+    public void ZoomToPlayer() {
+        StartCoroutine(1f.Tweeng((p)=>transform.position=p, transform.position, new Vector3(player.position.x,player.position.y + 0.2f * player.localScale.x, -10)));
+        StartCoroutine(1f.Tweeng((s)=>GetComponent<Camera>().orthographicSize=s, 5f, 2f));
+    }
+
+    [ContextMenu("ZoomOut")]
+    public void ZoomOutr() {
+        StartCoroutine(1f.Tweeng((p)=>transform.position=p, transform.position, new Vector3(0,0,-10)));
+        StartCoroutine(1f.Tweeng((s)=>GetComponent<Camera>().orthographicSize=s, 2f, 5f));
+    }
+}
